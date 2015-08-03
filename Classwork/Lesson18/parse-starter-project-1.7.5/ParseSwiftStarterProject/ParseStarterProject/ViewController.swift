5e5f3ae21b9fb98a9a403b32113ea3ee399b8600
kkane106 <kkane106@gmail.com>
2015-08-03 19:25:49 -0400

    Create something

---
+++ Classwork/Lesson18/parse-starter-project-1.7.5/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            println("Object has been saved.")
+        }
+    }
+
+}
+
